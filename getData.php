72ae7a5510b4bdbc5896e5d90cf6b836363c8df1
Lourdes <lirosalinas@gmail.com>
2016-05-28 11:58:32 +0200

    cambio de ubicacion de connect

--- getData.php
+++ getData.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
 	if($_SERVER['REQUEST_METHOD']=='GET'){
-		
+
 		$id  = $_GET['id'];
-		
-		require_once('dbConnect.php');
-		
+
+		require_once('../dbConnect.php');
+
 		$sql = "SELECT * FROM tableros";
-		
+
 		$r = mysqli_query($con,$sql);
-		
+
 		$res = mysqli_fetch_array($r);
-		
+
 		$result = array();
-		
+
 		array_push($result,array(
 			"nombre"=>$res['nombre'],
 			"nombre_equipo"=>$res['nombre_equipo']
 			)
 		);
-		
+
 		echo json_encode(array("result"=>$result));
-		
+
 		mysqli_close($con);
-		
+
 	}
