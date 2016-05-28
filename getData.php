b7dc5841cd51c84e4324b4a76cc2201aec0c69d0
Lourdes <lirosalinas@gmail.com>
2016-05-28 12:22:09 +0200

    vuelta atras

--- getData.php
+++ getData.php
@@ -9,27 +9,37 @@
     switch ($id) {
   	  case 1:
 		  	$sql = "SELECT * FROM tableros";
+				$r = mysqli_query($con,$sql);
+
+				$res = mysqli_fetch_array($r);
+
+				$result = array();
+
+				array_push($result,array(
+					"nombre"=>$res['nombre'],
+					"nombre_equipo"=>$res['nombre_equipo']
+					)
+				);
   		 break;
 
   	  default:
   			$sql = "SELECT * FROM equipos";
+				$r = mysqli_query($con,$sql);
+
+				$res = mysqli_fetch_array($r);
+
+				$result = array();
+
+				array_push($result,array(
+					"nombre_equipo"=>$res['nombre_equipo'],
+					"nombre_usu"=>$res['nombre_usu']
+					)
+				);
   		 break;
     }
 
-		$r = mysqli_query($con,$sql);
 
-		$res = mysqli_fetch_array($r);
 
-		$result = array();
-
-	/*	array_push($result,array(
-			"nombre"=>$res['nombre'],
-			"nombre_equipo"=>$res['nombre_equipo']
-			)
-		);*/
-		array_push($result,array(
-				$res
-				)
 
 		echo json_encode(array("result"=>$result));
 
