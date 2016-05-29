6e38bd43d37bb8b682d785cc160b05c9ef372917
Lourdes <lirosalinas@gmail.com>
2016-05-30 00:14:17 +0200

    ..

--- doCheck.php
+++ doCheck.php
@@ -1,6 +1,29 @@
 <?php
 header('Content-type: text/plain; charset=utf-8');
 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+	$id  = $_POST['id'];
+	$tick = $_POST['tick'];
+	$color = $_POST['color'];
+
+//importing database connection script
+ require_once('dbConnect.php');
+
+ //Creating sql query
+ $sql = "UPDATE tarjetas SET tick = $tick,  photo_id=$color WHERE id = $id";
+
+ //Updating database table
+ if (mysqli_query($con,$sql)) {
+ echo 'Successfully';
+ }
+ else {
+ echo 'Try Again';
+ }
+
+ //closing connection
+ mysqli_close($con);
+ }
 	/*if($_SERVER['REQUEST_METHOD']=='POST'){
 
     $id  = $_POST['id'];
@@ -20,20 +43,3 @@ echo "hoola";
         echo 'Error : ('. $mysqli->errno .') '. $mysqli->error;
     }
 */
-if($_SERVER['REQUEST_METHOD']=='POST'){
-$stmt = $mysqli->prepare("UPDATE tarjetas SET tick=?,
-   photo_id=?
-   WHERE id_tarjeta=?");
-$stmt->bind_param('iii',
-   $_POST['tick'],
-   $_POST['color'],
-   $_POST['id']);
-
-	 require_once('../dbConnect.php');
-
-	 $stmt->execute();
- }else{
-echo 'error';
-}
-
-		mysqli_close($con);
