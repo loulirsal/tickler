6664d8790828546f0d42b3492c75adee15adf7e5
Lourdes <lirosalinas@gmail.com>
2016-05-29 22:57:50 +0200

    arreglando errores de update

--- doCheck.php
+++ doCheck.php
@@ -9,6 +9,18 @@ header('Content-type: text/plain; charset=utf-8');
 
 		require_once('../dbConnect.php');
 
+    $statement = $mysqli->prepare("UPDATE tarjetas SET tick=?, photo_id=? WHERE id_tarjeta=?");
+
+    //bind parameters for markers, where (s = string, i = integer, d = double,  b = blob)
+    $statement->bind_param('ssi', $tick, $photo_id, $id);
+    $results =  $statement->execute();
+    if($results){
+        print 'Success! record updated';
+    }else{
+        print 'Error : ('. $mysqli->errno .') '. $mysqli->error;
+    }
+
+/*
   		//	$sql = "SELECT * FROM tarjetas WHERE id_ref ='".$id."'";
         $sql = "UPDATE tarjetas SET tick=".$tick.", photo_id=".$color." where id_tarjeta=".$id.;
 
@@ -17,7 +29,7 @@ header('Content-type: text/plain; charset=utf-8');
         } else {
           echo "Error updating record: " . mysqli_error($con);
         }
-
+*/
 
 		mysqli_close($con);
 
